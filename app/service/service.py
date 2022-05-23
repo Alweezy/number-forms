c2aafeaa8ea28d7f361f1566a158abf01eb55cb3
Alweezy <alvinmutisya@gmail.com>
2022-05-23 22:58:37 +0300

    Add logic

--- app/service/service.py
+++ app/service/service.py
@@ -1,6 +1,13 @@
+from app.service import errors
+
 def compute_output(value):
-  """This handles of the logic
-  input: @value: an integer
-  return: a string or numner depending on the value of the integer
-  """
-  pass
+
+  output = int(value)
+  if output % 7 == 0 and output % 5 == 0:
+    return "LR"
+  elif output % 5 == 0:
+    return "L"
+  elif output % 7 == 0:
+    return "R"
+  else:
+    return value
