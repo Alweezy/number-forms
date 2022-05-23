521004da570facf7a130cf39a3cd1a23fd220c24
Alweezy <alvinmutisya@gmail.com>
2022-05-23 23:11:07 +0300

    Implement and expose api

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_restful import Api
 
 from app.config import configurations
+from app.api.api import NumberForms
 
 
 def create_app(config_name):
   """create app factory defines app settings"""
 
-  # instanciate Flask using configs from the config file
   app = Flask(__name__, instance_relative_config=True)
   app.config.from_object(configurations[config_name])
+
+  # define API
+  api = Api(app)
+  api.add_resource(NumberForms, "/api/v1/number-forms")
 
   return app
