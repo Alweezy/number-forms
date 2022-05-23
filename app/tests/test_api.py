90c89edc88b570e364dd844f1217e18e80067041
Alweezy <alvinmutisya@gmail.com>
2022-05-23 23:17:50 +0300

    make use of http inbuilt status

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,6 +1,7 @@
 # integration tests 
 import json
 import pytest
+from http import HTTPStatus
 
 from app import create_app
 
@@ -29,9 +30,9 @@ def test_compute_output_happy_path(input, expected_response, expected_status_cod
 def test_compute_output_fails_given_non_integer_input(test_client_config):
   response = test_client_config.get("/api/v1/number-forms?number=non-integer-string")
   assert json.loads(response.data) == {"success": False, "error": "Invalid input. A numeric value was expected."}
-  assert response.status_code == 400
+  assert response.status_code == HTTPStatus.BAD_REQUEST
 
 def test_missing_number_query_arg(test_client_config):
   response = test_client_config.get("/api/v1/number-forms")
   assert json.loads(response.data) == {"success": False, "error": "argument number is required"}
-  assert response.status_code == 400
+  assert response.status_code == HTTPStatus.BAD_REQUEST
